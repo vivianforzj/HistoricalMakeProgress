b8f2ebf42c4d370f30715660e498b845db0ced9c
vivianforzj <zhoujing1990@gmail.com>
2014-04-25 15:47:34 +0800

    modify Demo.java
    Signed-off-by: vivianforzj <zhoujing1990@gmail.com>

--- src/git/Demo.java
+++ src/git/Demo.java
@@ -6,4 +6,13 @@ public class Demo {
 	public Demo(){
 		this.msg = "initialized!";
 	}
+	
+	public void printMSG(){
+		System.out.println(this.msg);
+	}
+	
+	public static void main(){
+		Demo d=new Demo();
+		d.printMSG();
+	}
 }
