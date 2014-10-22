98ac05633d57e0724c424f6915f7c35f18f2b1c4
jzhou <zhoujing1990@gmail.com>
2014-10-22 20:48:20 +0800

    Palindrome Number
    Signed-off-by: jzhou <zhoujing1990@gmail.com>

---
+++ LeetCode/src/main/java/easy/PalindromeNumber.java
@@ -0,0 +1,23 @@
+package main.java.easy;
+
+public class PalindromeNumber {
+
+	public static void main(String[] args) {
+		System.out.println(new PalindromeNumber().isPalindrome(222));
+	}
+
+
+    public boolean isPalindrome(int x) {
+    	if (x < 0) return false;
+    	if (x < 10) return true;
+    	long result = 0;
+    	int temp = x;
+    	while (x > 0) {
+    		result = result * 10 + x % 10;
+    		x /= 10;
+    	}
+    	if (temp == result) return true;
+		return false;
+    }
+
+}
--- LeetCode/src/main/java/easy/ReverseInteger.java
+++ LeetCode/src/main/java/easy/ReverseInteger.java
@@ -3,17 +3,32 @@ package main.java.easy;
 public class ReverseInteger {
 
 	public static void main(String[] args) {
-		System.out.println(new ReverseInteger().reverse(-2398506));
+		System.out.println(new ReverseInteger().reverse(1000000003));
+		System.out.println(new ReverseInteger().reverseFool(1000000003));
 	}
 	
-	public int reverse(int x) {
+	private int reverse(int i) {
+		long result = 0;
+		while (i != 0) {
+			result = result * 10 + i % 10;
+			i /= 10;
+		}
+		if (result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+		if (result > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+		return (int) result;
+	}
+
+	public int reverseFool(int x) {
 		StringBuffer sb = new StringBuffer();
 		String temp = String.valueOf(Math.abs(x));
 		
 		for( int i = temp.length() - 1; i >= 0; i--) {
 			sb.append(temp.charAt(i));
 		}
-		return x > 0 ? Integer.parseInt(sb.toString()) : -Integer.parseInt(sb.toString());
+		long result = x > 0 ? Long.parseLong(sb.toString()) : -Long.parseLong(sb.toString());
+		if (result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+		if (result > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+		return (int) result;
     }
 
 }
